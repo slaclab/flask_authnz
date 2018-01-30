aae2433ef96639a03fb802af2125f03537dcfaaa
Murali Shankar <mshankar@slac.stanford.edu>
2018-01-29 17:28:01 -0800

    Change to use run; this is only supported post 3.5 but seems to better suited to deal with exceptions

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='flask_authnz',
-      version='0.1',
+      version='0.0.8',
       description='A small utility for authentication/authorization for flask based services',
       url='https://github.com/slaclab/flask_authnz.git',
       author='Murali Shankar',
