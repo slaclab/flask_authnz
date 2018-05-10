9e5ad5e89721c0d6f7e19f1af4964a842ca2efe1
Murali Shankar <mshankar@slac.stanford.edu>
2018-05-10 16:29:57 -0700

    Update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='flask_authnz',
-      version='0.0.12',
+      version='0.0.14',
       description='A small utility for authentication/authorization for flask based services',
       url='https://github.com/slaclab/flask_authnz.git',
       author='Murali Shankar',
