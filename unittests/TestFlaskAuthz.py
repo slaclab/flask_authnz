0db76a037645d376f4b9c1ea25f46aa8bca4ff5a
Murali Shankar <mshankar@slac.stanford.edu>
2019-09-10 16:24:47 -0700

    Add instrument based permission unit tests

--- unittests/TestFlaskAuthz.py
+++ unittests/TestFlaskAuthz.py
@@ -3,8 +3,8 @@ import logging
 import sys
 import flask
 
-from flask_authnz.mongodb_dal import MongoDBRoles 
-from flask_authnz.flask_authnz import FlaskAuthnz 
+from flask_authnz.mongodb_dal import MongoDBRoles
+from flask_authnz.flask_authnz import FlaskAuthnz
 
 from werkzeug.exceptions import HTTPException
 
@@ -25,7 +25,7 @@ class MockUserGroups(object):
         self.user2groupsdict = user2groupsdict
     def get_user_posix_groups(self, user_id):
         return self.user2groupsdict.get(user_id, [])
-    
+
 class MockDatabase(object):
     def __init__(self, roledata):
         self.roledata = roledata
@@ -37,6 +37,8 @@ class MockDatabase(object):
                 ret.append(role)
         logger.debug("Returning %s for dict params %s", ret, params_dict)
         return ret
+    def find_one(self, params_dict):
+        return self.find(params_dict)[0]
 
 
 class TestFlaskAuthz(unittest.TestCase):
@@ -46,53 +48,72 @@ class TestFlaskAuthz(unittest.TestCase):
     """
     def test_group_has_editor(self):
         mgClient = {
-            "roles": {
-                "roles": MockDatabase( [ 
-                    { 
-                        "app" : "LogBook", 
-                        "name" : "Editor", 
-                        "privileges" : [ "read", "post", "manage_shifts", "edit", "delete" ], 
+            "site": {
+                "roles": MockDatabase( [
+                    {
+                        "app" : "LogBook",
+                        "name" : "Editor",
+                        "privileges" : [ "read", "post", "manage_shifts", "edit", "delete" ],
                         "players" : [ "uid:specific_global_editor", "ps_global_editors" ] },
                     {
-                        "app" : "LogBook", 
-                        "name" : "Reader", 
-                        "privileges" : [ "read"], 
-                        "players" : [ "uid:specific_global_reader", "ps_global_readers" ] 
+                        "app" : "LogBook",
+                        "name" : "Reader",
+                        "privileges" : [ "read"],
+                        "players" : [ "uid:specific_global_reader", "ps_global_readers" ]
+                    },
+                    {
+                        "app" : "LogBook",
+                        "name" : "Operator",
+                        "privileges" : [ "read", "post", "manage_shifts", "edit", "delete", "experiment_switch" ],
+                        "players" : [ "uid:PowerUser" ]
                     }
-                    ] )
+                    ] ),
+                "instruments": MockDatabase( [
+                    {
+                        "_id" : "XPP",
+                        "name" : "XPP",
+                        "roles": [
+                            {
+                                "app" : "LogBook",
+                                "name" : "Operator",
+                                "players" : [ "ps_xpp" ]
+                            } ],
+                    } ] )
                 },
              "xpp123456": {
-                "roles": MockDatabase( [ 
-                    { 
-                        "app" : "LogBook", 
-                        "name" : "Editor", 
+                "roles": MockDatabase( [
+                    {
+                        "app" : "LogBook",
+                        "name" : "Editor",
                         "players" : [ "uid:specific_xpp123456_editor", "ps_xpp123456_editors" ] },
                     {
-                        "app" : "LogBook", 
-                        "name" : "Reader", 
-                        "players" : [ "uid:specific_xpp123456_reader", "ps_xpp123456_readers" ] 
+                        "app" : "LogBook",
+                        "name" : "Reader",
+                        "players" : [ "uid:specific_xpp123456_reader", "ps_xpp123456_readers" ]
                     }
                     ] )
                 },
              "mec987654": {
-                "roles": MockDatabase( [ 
-                    { 
-                        "app" : "LogBook", 
-                        "name" : "Editor", 
+                "roles": MockDatabase( [
+                    {
+                        "app" : "LogBook",
+                        "name" : "Editor",
                         "players" : [ "uid:specific_mec987654_editor", "ps_mec987654_editors" ] },
                     {
-                        "app" : "LogBook", 
-                        "name" : "Reader", 
-                        "players" : [ "uid:specific_mec987654_reader", "ps_mec987654_readers" ] 
+                        "app" : "LogBook",
+                        "name" : "Reader",
+                        "players" : [ "uid:specific_mec987654_reader", "ps_mec987654_readers" ]
                     }
                     ] )
                 }
             }
         mkgp = MockUserGroups( {
-            "PowerUser" : ['ps_global_editors', 'ps_global_readers'], 
+            "PowerUser" : ['ps_global_editors', 'ps_global_readers'],
             "ReadOnlyUser" : ['ps_global_readers'],
             "xpp123456_PI" : ["ps_xpp123456_editors"],
-            "xpp123456_readonly" : ["ps_xpp123456_readers"]             
+            "xpp123456_readonly" : ["ps_xpp123456_readers"],
+            "xpp_instrment_operator": ["ps_xpp"],
+            "mec_instrment_operator": ["ps_mec"]
             } )
         dal = MongoDBRoles(mgClient, mkgp)
         security = FlaskAuthnz(dal, "LogBook")
@@ -102,28 +123,28 @@ class TestFlaskAuthz(unittest.TestCase):
         with app.test_request_context('/'):
             with self.assertRaises(HTTPException) as http_error:
                 self.assertRaises(Exception, security.authentication_required(part("Should not have authenticated...."))())
-                self.assertEqual(http_error.exception.code, 403)            
-            
+                self.assertEqual(http_error.exception.code, 403)
+
             flask.request.environ["HTTP_REMOTE_USER"] = "ReadOnlyUser"
             self.assertTrue(security.authentication_required(part("Authenticated...."))())
             self.assertTrue(security.authorization_required("read")(part)("Authorized", **{'experiment_name':'xpp123456'}))
             with self.assertRaises(HTTPException) as http_error:
                 self.assertFalse(security.authorization_required("edit")(part)("Authorized", **{'experiment_name':'xpp123456'}))
                 self.assertTrue(security.authorization_required("read")(part)("Authorized", **{'experiment_name':'xpp123456'}))
-                self.assertEqual(http_error.exception.code, 403)            
+                self.assertEqual(http_error.exception.code, 403)
 
             flask.request.environ["HTTP_REMOTE_USER"] = "specific_xpp123456_reader"
             self.assertTrue(security.authentication_required(part("Authenticated...."))())
             self.assertTrue(security.authorization_required("read")(part)("Authorized", **{'experiment_name':'xpp123456'}))
             with self.assertRaises(HTTPException) as http_error:
                 self.assertFalse(security.authorization_required("edit")(part)("Authorized", **{'experiment_name':'xpp123456'}))
-                self.assertEqual(http_error.exception.code, 403)            
+                self.assertEqual(http_error.exception.code, 403)
             with self.assertRaises(HTTPException) as http_error:
                 self.assertFalse(security.authorization_required("read")(part)("Authorized", **{'experiment_name':'mec987654'}))
-                self.assertEqual(http_error.exception.code, 403)            
+                self.assertEqual(http_error.exception.code, 403)
             with self.assertRaises(HTTPException) as http_error:
                 self.assertFalse(security.authorization_required("edit")(part)("Authorized", **{'experiment_name':'mec987654'}))
-                self.assertEqual(http_error.exception.code, 403)            
+                self.assertEqual(http_error.exception.code, 403)
 
             flask.request.environ["HTTP_REMOTE_USER"] = "xpp123456_PI"
             self.assertTrue(security.authentication_required(part("Authenticated...."))())
@@ -131,7 +152,21 @@ class TestFlaskAuthz(unittest.TestCase):
             self.assertTrue(security.authorization_required("edit")(part)("Authorized", **{'experiment_name':'xpp123456'}))
             with self.assertRaises(HTTPException) as http_error:
                 self.assertFalse(security.authorization_required("read")(part)("Authorized", **{'experiment_name':'mec987654'}))
-                self.assertEqual(http_error.exception.code, 403)            
+                self.assertEqual(http_error.exception.code, 403)
             with self.assertRaises(HTTPException) as http_error:
                 self.assertFalse(security.authorization_required("edit")(part)("Authorized", **{'experiment_name':'mec987654'}))
-                self.assertEqual(http_error.exception.code, 403)            
+                self.assertEqual(http_error.exception.code, 403)
+
+        with app.test_request_context('/'):
+            # Test instrument only privileges
+            flask.g.instrument = "XPP"
+            flask.request.environ["HTTP_REMOTE_USER"] = "xpp_instrment_operator"
+            self.assertTrue(security.authentication_required(part("Authenticated...."))())
+            self.assertTrue(security.authorization_required("experiment_switch")(part)("Authorized"))
+
+        with app.test_request_context('/'):
+            flask.request.environ["HTTP_REMOTE_USER"] = "mec_instrment_operator"
+            self.assertTrue(security.authentication_required(part("Authenticated...."))())
+            with self.assertRaises(HTTPException) as http_error:
+                self.assertFalse(security.authorization_required("experiment_switch")(part)("Authorized"))
+                self.assertEqual(http_error.exception.code, 403)
